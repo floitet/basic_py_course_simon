e58e64254b0d7c3f05f38c0df08bc44520366f4f
Simon Troyanovskiy <floitet@yandex.ru>
2020-02-03 18:03:34 +0300

    tasks done for lesson06

--- task_6.1.py
+++ task_6.1.py
@@ -1,1 +1,35 @@
 # Task 6.1
+
+from time import sleep
+from itertools import cycle
+import datetime
+
+
+class TrafficLight:
+    def __init__(self, go, wait, stop):
+        self.__color = [go, wait, stop]
+
+    def running(self):
+        t = str(datetime.datetime.time(datetime.datetime.now()))
+        correct_order = ['red', 'yellow', 'green']
+        new_list = [i for i in self.__color if i == correct_order[self.__color.index(i)]]
+        if len(new_list) < 3:
+            print('Wrong order')
+        else:
+            self.__color.append('yellow')
+            for i in cycle(self.__color):
+                if i == 'yellow':
+                    print(f'\033[33m{i}')
+                    sleep(2)
+                elif i == 'red':
+                    print(f'\033[31m{i}')
+                    sleep(7)
+                else:
+                    print(f'\033[32m{i}')
+                    sleep(7)
+                if 0 <= int(t[:2]) <= 5:
+                    break
+
+
+a = TrafficLight('red', 'yellow', 'green')
+a.running()
--- task_6.5.py
+++ task_6.5.py
@@ -1,1 +1,51 @@
 # Task 6.5
+# Реализовать класс Stationery (канцелярская принадлежность).
+# Определить в нем атрибут title (название) и метод draw (отрисовка). Метод выводит сообщение “Запуск отрисовки.”
+# Создать три дочерних класса Pen (ручка), Pencil (карандаш), Handle (маркер).
+# В каждом из классов реализовать переопределение метода draw.
+# Для каждого из классов метод должен выводить уникальное сообщение.
+# Создать экземпляры классов и проверить, что выведет описанный метод для каждого экземпляра.
+
+class Stationery:
+    def __init__(self, title):
+        self.title = title
+
+    def draw(self):
+        print(self.title)
+        print('Launching drawing.')
+
+obj = Stationery('Unknown object.')
+obj.draw()
+
+class Pen(Stationery):
+    def __init__(self, title):
+        super().__init__(title)
+
+    def draw(self):
+        print(f'Working on an {self.title}.')
+        print('Writing a word.')
+
+gel_black_pen = Pen('essay')
+gel_black_pen.draw()
+
+class Pencil(Stationery):
+    def __init__(self, title):
+        super().__init__(title)
+
+    def draw(self):
+        print(f'Working on a {self.title}.')
+        print('Drawing a thin line.')
+
+sharp_pencil = Pencil('portret')
+sharp_pencil.draw()
+
+class Marker(Stationery):
+    def __init__(self, title):
+        super().__init__(title)
+
+    def draw(self):
+        print(f'Working on a {self.title}.')
+        print('Highlighting important info.')
+
+red_thick_marker = Marker('summary')
+red_thick_marker.draw()
