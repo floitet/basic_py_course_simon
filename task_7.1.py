25bd7cae1d41c6d0bb85ea14a475c9c1b78e6720
Simon Troyanovskiy <floitet@yandex.ru>
2020-02-06 10:50:15 +0300

    tasks done for lesson07

--- task_7.1.py
+++ task_7.1.py
@@ -1,1 +1,45 @@
 # Task 7.1
+import random
+
+# можно менять количество строк и рядов
+# скрипт будет создавать два списка списков с рандомными числами в нужном количестве
+
+
+lines = 10
+num_in_line = 10
+sample_list = [random.sample(range(100), num_in_line) for i in range(lines)]
+sample_list2 = [random.sample(range(100), num_in_line) for i1 in range(lines)]
+
+
+class Matrix:
+    def __init__(self, matrix_list):
+        self.matrix_list = matrix_list
+
+    def __str__(self):
+
+        for i in self.matrix_list:
+            line = f'| '
+            for k in i:
+                line += f'{str(k):3} '
+            line += ' |'
+            print(line)
+
+        return f'Here is your matrix.'
+
+    # при суммировании матриц создается новая переменная,
+    # с помощью которой затем создается новый экземпляр класса Matrix
+    # суммирование происходит с помощью двух генераторов в одну строчку
+
+    def __add__(self, other):
+        new_matrix = [[x + y for x, y in zip(i, k)] for i, k in zip(self.matrix_list, other.matrix_list)]
+        return Matrix(new_matrix)
+
+
+a = Matrix(sample_list)
+b = Matrix(sample_list2)
+print(a.matrix_list)
+print(b.matrix_list)
+print(a)
+print(b)
+c = (a + b)
+print(c)
--- task_7.3.py
+++ task_7.3.py
@@ -1,1 +1,66 @@
 # Task 7.3
+class Cell:
+    def __init__(self, cellule):
+        try:
+            self.cellule = int(cellule)
+            if cellule <= 0 or isinstance(cellule, float):
+                raise ValueError
+
+        except ValueError:
+            print('This parameter should be positive integer number.')
+
+    def make_order(self, cell, num_in_row):
+        new_line = f''
+        cell = cell.cellule
+        while cell > 0:
+            if cell > num_in_row:
+                new_line += f'{"*" * num_in_row}\n'
+                cell -= num_in_row
+            else:
+                new_line += f'{"*" * cell}\n'
+                cell -= num_in_row
+        return new_line
+
+    def __add__(self, other):
+        adding = self.cellule + other.cellule
+        return Cell(adding)
+
+    def __sub__(self, other):
+        try:
+            subtracting = self.cellule - other.cellule
+            if self.cellule <= other.cellule:
+                raise ValueError
+            return Cell(subtracting)
+
+        except ValueError:
+            print('First attribute should be higher than second.\n'
+                  'Biological cell can not occupy less than 1 cellule.')
+
+    def __mul__(self, other):
+        multiplying = self.cellule * other.cellule
+        return Cell(multiplying)
+
+    def __truediv__(self, other):
+        truedivision = int(self.cellule / other.cellule)
+        return Cell(truedivision)
+
+
+try:
+    a = Cell(11)
+    b = Cell(5)
+    c = (b + a)
+    print(f'The new cell after summing up occupies {c.cellule} cellules.')
+    d = (a - b)
+    print(f'The new cell after subtracting occupies {d.cellule} cellules.')
+    e = (a * b)
+    print(f'The new cell after multiplying occupies {e.cellule} cellules.')
+    f = (a / b)
+    print(f'The new cell after division occupies {f.cellule} cellules.')
+    print(b.make_order(a, 6))
+
+except ZeroDivisionError:
+    print('One of the parameters you typed in was "0".\n'
+          'There were multiple mistakes in running the code.')
+
+except AttributeError:
+    print('One of the mathematical operations went wrong.')
